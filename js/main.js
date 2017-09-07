4480e139b4718d82f08bc58acdda36b7d7f714a9
e1bandito <30836258+e1bandito@users.noreply.github.com>
2017-09-07 15:30:38 +0300

    Валидация формы и автопрефиксер.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
+// Появление и скрытие формы - начало.
 var link = document.querySelector(".search-form-button");
 var hotelSearch = document.querySelector(".hotels-search");
 var hotelsSearchToggle = document.querySelector("hotels-search-toggle");
-var adult = document.querySelector("[name=adult]");
-var adultValue = 2;
-var adultPlus = document.querySelector(".button-adult-plus");
-var adultMinus = document.querySelector(".button-adult-minus");
-var child = document.querySelector("[name=child]");
-var childValue = 0;
-var childPlus = document.querySelector(".button-child-plus");
-var childMinus = document.querySelector(".button-child-minus");
 
 if ("hotelsSearchToggle") {
 	hotelSearch.classList.add("hotels-search-toggle");
@@ -17,7 +10,36 @@ if ("hotelsSearchToggle") {
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	hotelSearch.classList.toggle("hotels-search-toggle");
+	hotelSearch.classList.remove("form-error");
 });
+// Появление и скрытие формы - конец.
+
+//Валидация формы - начало.
+
+var searchForm = document.querySelector("form");
+var dateIn = document.querySelector("[name=date-in]");
+var dateOut = document.querySelector("[name=date-out]");
+
+
+searchForm.addEventListener("submit", function (evt) {
+	if (!dateIn.value || !dateOut.value) {
+    evt.preventDefault();
+	hotelSearch.classList.remove("form-error");
+    hotelSearch.offsetWidth = hotelSearch.offsetWidth;
+	hotelSearch.classList.add("form-error");
+	dateIn.focus();
+		if (dateIn.value) {
+			dateOut.focus();
+		}
+	}
+  });
+//Валидация формы - конец.
+
+// Плюс и минус взрослые - начало.
+var adult = document.querySelector("[name=adult]");
+var adultValue = 2;
+var adultPlus = document.querySelector(".button-adult-plus");
+var adultMinus = document.querySelector(".button-adult-minus");
 
 adultPlus.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -33,6 +55,14 @@ adultMinus.addEventListener("click", function (evt) {
 	};
 	adult.value = adultValue;
 });
+// Плюс и минус взрослые - конец.
+
+// Плюс и минус дети - начало.
+var child = document.querySelector("[name=child]");
+var childValue = 0;
+var childPlus = document.querySelector(".button-child-plus");
+var childMinus = document.querySelector(".button-child-minus");
+
 
 childPlus.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -48,3 +78,5 @@ childMinus.addEventListener("click", function (evt) {
     }
 	child.value = childValue;
 });
+
+// Плюс и минус дети - конец.
